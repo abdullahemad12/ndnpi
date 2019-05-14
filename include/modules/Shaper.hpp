688bc8e7b0aaaf5d23d8aea6599a68755367a820
Abdullah Emad <abdullahem1997@hotmail.com>
2019-05-14 15:04:45 +0200

    Create the shaper class

---
+++ include/modules/Shaper.hpp
@@ -0,0 +1,50 @@
+/**
+  * MIT License
+  *
+  * Copyright (c) 2019 Abdullah Emad
+  * 
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  * 
+  * The above copyright notice and this permission notice shall be included in all
+  * copies or substantial portions of the Software.
+  * 
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
+  */
+
+#ifndef _MODULES_SHAPER_
+#define _MODULES_SHAPER_
+
+
+#include <queue>
+#include <mutex>
+#include <ndn-cxx/face.hpp>
+
+#define N_PRIORITIES 4
+
+
+using namespace std;
+
+class Shaper
+{
+	private:
+		mutex lock;
+		queue<ndn::Interest> shaping_queues[N_PRIORITIES];
+	public:
+		Shaper();
+	
+};
+
+
+#endif /*..._MODULES_SHAPER_*/
+
